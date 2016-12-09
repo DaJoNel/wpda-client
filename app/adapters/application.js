cce8228f113d2adde1d0104e0d15eb120bde65fe
David Nelson <dajonel@icloud.com>
2016-12-09 03:29:13 -0600

    Updated template. Removed simpleauth in favor of a more immediate solution.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+export default DS.JSONAPIAdapter.extend({
 	namespace: 'api',
 	authorizer: 'authorizer:application',
 
